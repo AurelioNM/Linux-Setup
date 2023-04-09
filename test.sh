37adaf3084c0983ebb7311afba9a44d1abe6194c
AurelioNM <aureliomororo300@gmail.com>
2023-04-08 22:46:38 -0300

    add kickstart setup to nvim

--- test.sh
+++ test.sh
@@ -4,11 +4,9 @@ echo 'NVIM setup!'
 
 #update
 sudo apt-get update -y
-sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
-sudo apt autoremove -y
+sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip -y
 
-
-#nvim
+#build nvim
 mkdir git
 cd git
 git clone https://github.com/neovim/neovim.git
@@ -16,6 +14,17 @@ cd neovim
 make CMAKE_BUILD_TYPE=Release
 sudo make install
 cd
+
+echo 'KICKSTART setup!'
+
+#config kickstart nvim
+mkdir -p .config/nvim
+cd .config/nvim
+git clone git@github.com:nvim-lua/kickstart.nvim.git
+cd kickstart.nvim 
+mv * ..
+cd ..
+rm -rf cd kickstart.nvim
 
 echo 'Done!'
 
